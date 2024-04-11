f551e1b0e2c3277a667a0f488d4753ec5fef9ea8
0shreyajha0 <0shreyajha0@gmail.com>
2024-04-11 10:53:18 +0530

    updated recipe detail component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,18 +12,18 @@ export const Home = () => {
   const [recipeList, setRecipeList] = useState([]);
 
   useEffect(() => {
-    // const response = fetch(
-    //   "https://api.spoonacular.com/recipes/random?number=12&apiKey=3f10eba91f56409d8c16d5fe42295498"
-    // );
-    // response
-    //   .then((res) => res.json())
-    //   .then((data) => {
-    //     console.log(data.recipes);
-    //     setRecipeList(data.recipes);
-    //   });
+    const response = fetch(
+      "https://api.spoonacular.com/recipes/random?number=12&apiKey=3f10eba91f56409d8c16d5fe42295498"
+    );
+    response
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.recipes);
+        setRecipeList(data.recipes);
+      });
 
-    //This is temporary
-    setRecipeList(MOCK_DATA);
+    // //This is temporary
+    // setRecipeList(MOCK_DATA);
   }, []);
 
   return (
